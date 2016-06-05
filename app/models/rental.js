d55442d37453decc4b4784821781986b4ea4c505
gogi <grobelska.m@gmail.com>
2016-06-05 21:53:21 +0200

    Add attributes to rental model

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,5 +1,10 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
-  
+  title: attr(),
+  owner: attr(),
+  city: attr(),
+  type: attr(),
+  bedrooms: attr(),
+  image: attr()
 });
