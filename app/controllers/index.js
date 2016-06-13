cde6d651c2d1e5981f42f68997b266d5c11ba5c5
gogi <grobelska.m@gmail.com>
2016-06-13 23:31:50 +0200

    Add #autoComplete and #search to newly created controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  filteredList: null,
+  actions: {
+    autoComplete(param) {
+      if (param !== '') {
+        this.get('store').query('rental', {city.param}).then((result) => this.set('filteredList', result));
+      } else {
+        this.set('filteredList', null);
+      }
+    }
+  }
 });
